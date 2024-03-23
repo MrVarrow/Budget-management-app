de97ad89c4a88fe230fc4810a8cf2db884aa22a9
MrVarrow <77280837+MrVarrow@users.noreply.github.com>
2024-03-23 18:04:49 +0100

    Logged user page

--- LoggedUserPage/LoggedUserPageController.py
+++ LoggedUserPage/LoggedUserPageController.py
@@ -1,21 +1,27 @@
 from LoggedUserPage.LoggedUserPageView import LoggedUserPageView
 from LoggedUserPage.LoggedUserPageModel import LoggedUserPageModel
+from SettingsPage.SettingsPageController import SettingsPageController
+from AccountPage.AccountPageController import AccountPageController
+from RateAppWindow.RateAppWindowController import RateAppWindowController
+from MobileAppWindow.MobileAppWindowController import MobileAppWindowController
+
 from tkinter import messagebox
 
 
 class LoggedUserPageController:
-    def __init__(self, root):
+    def __init__(self, root, user_data):
         self.root = root
+        self.user_data = user_data
         self.logged_user_page_view = LoggedUserPageView(self.root, self)
         self.logged_user_page_model = LoggedUserPageModel()
 
     # Go into settings
     def settings(self):
-        ...
+        SettingsPageController(self.root, self.user_data)
 
     # Go into user account
     def your_acc(self):
-        ...
+        AccountPageController(self.root, self.user_data)
 
     # Go into manage budget page
     def manage_budget(self):
@@ -39,11 +45,11 @@ class LoggedUserPageController:
 
     # Go into rate us window
     def rate_us(self):
-        ...
+        RateAppWindowController(self.root, self.user_data)
 
     # Go into mobile app window
     def mobile_app(self):
-        ...
+        MobileAppWindowController(self.root, self.user_data)
 
     # Display question if user wants to log out, then takes action based on decision
     def logout(self):
