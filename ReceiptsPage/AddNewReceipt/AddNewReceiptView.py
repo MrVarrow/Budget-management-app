1efe6ba538892b851a32051a758587ed605b98e2
MrVarrow <77280837+MrVarrow@users.noreply.github.com>
2024-04-16 20:56:38 +0200

    Add files via upload

--- ReceiptsPage/AddNewReceipt/AddNewReceiptView.py
+++ ReceiptsPage/AddNewReceipt/AddNewReceiptView.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
-from pandastable import Table, TableModel
 import pandas as pd
+
 
 class AddNewReceiptView:
     def __init__(self, master, controller, bg_color):
@@ -18,46 +18,46 @@ class AddNewReceiptView:
         self.items_frame.grid(row=1, rowspan=6, column=1)
 
         # Labels and entries
-        Label(self.add_new_receipt_frame, text="Add new Receipt") \
-            .grid(row=0, column=0, columnspan=3)
+        Label(self.add_new_receipt_frame, text="Add new Receipt", font=('Arial', 40), bg='light gray') \
+            .grid(row=0, column=0, columnspan=3, sticky=EW, padx=225, pady=40, ipadx=210, ipady=50)
 
-        Label(self.add_new_receipt_frame, text="Enter product name:") \
-            .grid(row=1, column=0)
-        Entry(self.add_new_receipt_frame) \
-            .grid(row=2, column=0)
+        Label(self.add_new_receipt_frame, text="Enter product name:", font=('Arial', 15)) \
+            .grid(row=1, column=0, sticky=W, padx=50)
+        Entry(self.add_new_receipt_frame, font=('Arial', 20)) \
+            .grid(row=2, column=0, sticky=W, padx=50)
 
-        Label(self.add_new_receipt_frame, text="Enter product price:") \
-            .grid(row=3, column=0)
-        Entry(self.add_new_receipt_frame) \
-            .grid(row=4, column=0)
+        Label(self.add_new_receipt_frame, text="Enter product price:", font=('Arial', 15)) \
+            .grid(row=3, column=0, sticky=W, padx=50)
+        Entry(self.add_new_receipt_frame, font=('Arial', 20)) \
+            .grid(row=4, column=0, sticky=W, padx=50)
 
-        Label(self.add_new_receipt_frame, text="Try by uploading a photo:\n"
+        Label(self.add_new_receipt_frame, font=('Arial', 15), text="Try by uploading a photo:\n"
                                                "Choose photo:") \
-            .grid(row=1, column=2)
+            .grid(row=1, column=2, sticky=E, padx=65)
 
         # Submit item button
-        Button(self.add_new_receipt_frame, text="Submit") \
-            .grid(row=5, column=0)
+        Button(self.add_new_receipt_frame, text="Submit", font=('Arial', 20), bg="light gray", width=10) \
+            .grid(row=5, column=0, sticky=W, padx=120, pady=20)
 
         # Delete item button
-        Button(self.add_new_receipt_frame, text="Delete") \
-            .grid(row=6, column=0)
+        Button(self.add_new_receipt_frame, text="Delete", font=('Arial', 20), bg="light gray", width=10) \
+            .grid(row=6, column=0, sticky=W, padx=120, pady=20)
 
         # Add receipt button
-        Button(self.add_new_receipt_frame, text="Add receipt") \
-            .grid(row=7, column=1)
+        Button(self.add_new_receipt_frame, text="Add receipt", font=('Arial', 20), bg="light gray", width=10) \
+            .grid(row=7, column=1, pady=10)
 
         # Choose path to receipt photo button
-        Button(self.add_new_receipt_frame, text="path to selected file") \
-            .grid(row=2, column=2)
+        Button(self.add_new_receipt_frame, text="path to selected file", font=('Arial', 20), bg="light gray") \
+            .grid(row=2, rowspan=2, column=2, sticky=SE, padx=50)
 
         # Submit photo button
-        Button(self.add_new_receipt_frame, text="Submit") \
-            .grid(row=3, column=2)
+        Button(self.add_new_receipt_frame, text="Submit", font=('Arial', 20), bg="light gray", width=10) \
+            .grid(row=4, rowspan=2, column=2, sticky=SE, padx=100)
 
         # Back to Receipt page
-        Button(self.add_new_receipt_frame, text="Back") \
-            .grid(row=7, column=2)
+        Button(self.add_new_receipt_frame, text="Back", font=('Arial', 15), bg="light gray", width=7) \
+            .grid(row=8, column=2, sticky=E)
 
         data = {'Name': ['John', 'Jane', 'Bob', 'Alice', 'Tom', 'Lily', 'Mike', 'Emily'],
         'Age': [25, 30, 35, 40, 45, 50, 55, 60],
@@ -65,7 +65,7 @@ class AddNewReceiptView:
         df = pd.DataFrame(data)
 
         # Table for items
-        table = ttk.Treeview(self.items_frame, height=3)
+        table = ttk.Treeview(self.items_frame, height=15)
         table["columns"] = list(df.columns)
         table.column("#0", width=100)
         table.pack(fill="both", expand=True)
@@ -78,3 +78,5 @@ class AddNewReceiptView:
         table.configure(yscrollcommand=scrollbar.set)
         for i, row in df.iterrows():
             table.insert("", "end", text=i, values=list(row))
+
+
