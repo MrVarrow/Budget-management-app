bdbf6e99c6ceb10e6f72ff770b107ac4bc879175
MrVarrow <77280837+MrVarrow@users.noreply.github.com>
2024-06-09 16:44:18 +0200

    Add files via upload

--- CreateAccountPage/CreateAccountView.py
+++ CreateAccountPage/CreateAccountView.py
@@ -5,6 +5,7 @@ class CreateAccountPageView:
     def __init__(self, master, controller, bg_color):
         self.controller = controller
         self.root = master
+        self.bg_color = bg_color
 
         # Define variables for entries and checkbutton
         self.email_notification_state = BooleanVar()
@@ -14,7 +15,7 @@ class CreateAccountPageView:
         self.email_var = StringVar()
 
         # Frame creation
-        self.create_account_page = Frame(self.root, bg=bg_color)
+        self.create_account_page = Frame(self.root, bg=self.bg_color)
         self.create_account_page.grid(row=0, column=0, sticky=NSEW)
 
         # Labels and Entries
@@ -37,29 +38,30 @@ class CreateAccountPageView:
                                                                                                 "strong") \
             .grid(row=4, column=1, rowspan=3, columnspan=2, sticky=SE, padx=300, pady=25, ipadx=20, ipady=20)
 
-        Label(self.create_account_page, text="login:", font=('Arial', 20)) \
+        Label(self.create_account_page, text="login:", font=('Arial', 20), bg=self.bg_color) \
             .grid(row=1, column=1, padx=450, sticky=W)
         Entry(self.create_account_page, font=('Arial', 20), textvariable=self.login_var) \
             .grid(row=2, column=1, padx=450, sticky=W, pady=10)
 
-        Label(self.create_account_page, text="password:", font=('Arial', 20)) \
+        Label(self.create_account_page, text="password:", font=('Arial', 20), bg=self.bg_color) \
             .grid(row=3, column=1, padx=450, sticky=W)
         Entry(self.create_account_page, font=('Arial', 20), textvariable=self.password_var) \
             .grid(row=4, column=1, padx=450, sticky=W, pady=10)
 
-        Label(self.create_account_page, text="repeat password:", font=('Arial', 20)) \
+        Label(self.create_account_page, text="repeat password:", font=('Arial', 20), bg=self.bg_color) \
             .grid(row=5, column=1, padx=450, sticky=W)
         Entry(self.create_account_page, font=('Arial', 20), textvariable=self.repeat_password_var) \
             .grid(row=6, column=1, padx=450, sticky=W, pady=10)
 
-        Label(self.create_account_page, text="e-mail:", font=('Arial', 20)) \
+        Label(self.create_account_page, text="e-mail:", font=('Arial', 20), bg=self.bg_color) \
             .grid(row=7, column=1, padx=450, sticky=W)
         Entry(self.create_account_page, font=('Arial', 20), textvariable=self.email_var) \
             .grid(row=8, column=1, padx=450, sticky=W, pady=10)
 
         # E-mail notifications checkbutton
         Checkbutton(self.create_account_page, text="I want to get e-mail notifications about news", font=('Arial', 12),
-                    variable=self.email_notification_state, onvalue=True, offvalue=False, command=lambda: self.controller.dark_mode()) \
+                    variable=self.email_notification_state, onvalue=True, offvalue=False, bg=self.bg_color,
+                    command=lambda: self.controller.dark_mode()) \
             .grid(row=8, column=1, columnspan=2, sticky=E, padx=150)
 
         # Create account button
