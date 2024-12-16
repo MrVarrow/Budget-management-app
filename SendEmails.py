cf8cc435870c779144e2d99652e0080dd6968609
Franciszek Gawadziński <77280837+MrVarrow@users.noreply.github.com>
2024-12-16 18:56:21 +0100

    Add files via upload

--- SendEmails.py
+++ SendEmails.py
@@ -3,8 +3,6 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-# For readability in future make class with methods and put in separate folder also change imports in files that
-# use this module to single function imports (which are they using) for optimization
 def send_email(sender_email, sender_password, receiver_email, subject, message):
     global server
     smtp_server = 'smtp.gmail.com'
@@ -39,18 +37,7 @@ def send_confirm_email(email_input):
     send_email(sender_email, sender_password, receiver_email, subject, message)
 
 
-def send_email_verification(self, receiver, code):
-    sender_email = 'budgetappofficial@gmail.com'
-    sender_password = ''  # for safety reasons leaving empty
-    receiver_email = '{}'.format(receiver)
-    subject = 'E-mail verification'
-    message = 'To verify your e-mail type this code into app:\n' \
-              '{}'.format(code)
-
-    send_email(sender_email, sender_password, receiver_email, subject, message)
-
-
-def forgot_password_email(self, receiver, password):
+def forgot_password_email(receiver, password):
     sender_email = 'budgetappofficial@gmail.com'
     sender_password = ''  # for safety reasons leaving empty
     receiver_email = '{}'.format(receiver)
@@ -70,3 +57,101 @@ def send_email_with_link(receiver, link):
               '{}'.format(link)
 
     send_email(sender_email, sender_password, receiver_email, subject, message)
+
+
+def send_email_with_code(receiver, code):
+    sender_email = 'budgetappofficial@gmail.com'
+    sender_password = ''  # for safety reasons leaving empty
+    receiver_email = '{}'.format(receiver)
+    subject = 'Did you forget your password?'
+    message = 'Here is your code to verify your e-mail:\n' \
+              '{}'.format(code)
+
+    send_email(sender_email, sender_password, receiver_email, subject, message)
+
+# to impelent
+def send_email_with_email_change_info(receiver, username, new_email):
+    sender_email = 'budgetappofficial@gmail.com'
+    sender_password = ''  # for safety reasons leaving empty
+    receiver_email = '{}'.format(receiver)
+    subject = 'E-mail changed.'
+    message = 'You had changed your E-mail for account {} to\n' \
+              '{}'.format(username, new_email)
+
+    send_email(sender_email, sender_password, receiver_email, subject, message)
+# to impelent
+def send_email_with_delete_acc(receiver, username):
+    sender_email = 'budgetappofficial@gmail.com'
+    sender_password = ''  # for safety reasons leaving empty
+    receiver_email = '{}'.format(receiver)
+    subject = 'Account Deleted.'
+    message = 'You had deleted your account with username\n' \
+              '{}, if you want you can tell us why did you decided to do so in reply to this E-mail. Thanks!'.format(username)
+
+    send_email(sender_email, sender_password, receiver_email, subject, message)
+
+# to impelent
+def send_email_with_clear_data(receiver, username):
+    sender_email = 'budgetappofficial@gmail.com'
+    sender_password = ''  # for safety reasons leaving empty
+    receiver_email = '{}'.format(receiver)
+    subject = 'All data from your account has been cleaned.'
+    message = 'You had cleaned data from your account with username\n' \
+              '{}, if you want you can tell us why did you decided to do so in reply to this E-mail. Thanks!'.format(
+        username)
+
+    send_email(sender_email, sender_password, receiver_email, subject, message)
+
+
+# OPTIONAL
+
+# to impelent
+def send_email_when_saving_goal_full(receiver, goal_name):
+    sender_email = 'budgetappofficial@gmail.com'
+    sender_password = ''  # for safety reasons leaving empty
+    receiver_email = '{}'.format(receiver)
+    subject = 'Good job! Your goal is full.'
+    message = 'Your goal: {} is full make sure to withdraw money and set new goals.'.format(goal_name)
+
+    send_email(sender_email, sender_password, receiver_email, subject, message)
+# to impelent
+def send_email_to_rate_the_app(receiver, username):
+    sender_email = 'budgetappofficial@gmail.com'
+    sender_password = ''  # for safety reasons leaving empty
+    receiver_email = '{}'.format(receiver)
+    subject = 'Rate budget app.'
+    message = 'Hi, {} We will be glad if you would like to rate our app, you can do this directly in app by clicking' \
+              'rate app button in main menu. Thanks!'.format(username)
+
+    send_email(sender_email, sender_password, receiver_email, subject, message)
+
+# to impelent
+def send_email_thanks_for_rate(receiver, username):
+    sender_email = 'budgetappofficial@gmail.com'
+    sender_password = ''  # for safety reasons leaving empty
+    receiver_email = '{}'.format(receiver)
+    subject = 'We want to thank you for your time.'
+    message = 'Hi, {} thanks for rating our app!'.format(username)
+
+    send_email(sender_email, sender_password, receiver_email, subject, message)
+# to impelent
+def send_email_when_user_was_inactive(receiver, username):
+    sender_email = 'budgetappofficial@gmail.com'
+    sender_password = ''  # for safety reasons leaving empty
+    receiver_email = '{}'.format(receiver)
+    subject = 'We got some news for you in budget app'
+    message = 'Hi, {}!\n We havent seen each other for a long time. We miss you :( Come back and see whats new' \
+              'in the app and keep tracking your finances.'.format(username)
+
+    send_email(sender_email, sender_password, receiver_email, subject, message)
+# to impelent
+def send_email_with_reminder_to_set_budget(receiver, username):
+    sender_email = 'budgetappofficial@gmail.com'
+    sender_password = ''  # for safety reasons leaving empty
+    receiver_email = '{}'.format(receiver)
+    subject = 'Dont miss your budget for incoming month!'
+    message = 'Hi, {} remember to set budget for next month. Dont let your finances take control ' \
+              'over you'.format(username)
+
+    send_email(sender_email, sender_password, receiver_email, subject, message)
+
